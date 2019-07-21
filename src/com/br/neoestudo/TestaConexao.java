25a82e00ab7ffa90b741ffbac3aa2f0f6e54e586
Nesomar <nesomar.ramalho@outlook.com>
2019-07-21 19:33:35 -0300

    ajuste no test conexão

--- src/com/br/neoestudo/TestaConexao.java
+++ src/com/br/neoestudo/TestaConexao.java
@@ -5,21 +5,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class TestaConexao {
 
-	public static void main(String[] args) throws SQLException {
-		
-		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/loja_virtual", "postgres", "1234");
-		System.out.println("Abrindo a conexão com o banco.");
-		
-		Statement statement = connection.createStatement();
-		ResultSet produtos = statement.executeQuery("SELECT * FROM Produto");
-		
-		while (produtos.next()) {
-			System.out.println("Nome do Produto:" + produtos.getString("nome"));
-			
+	public static void main(String[] args) {
+
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/loja_virtual",
+				"postgres", "1234"); Statement statement = connection.createStatement()) {
+			System.out.println("Abrindo a conexão com o banco.");
+			ResultSet produtos = statement.executeQuery("SELECT * FROM Produto");
+
+			while (produtos.next()) {
+				System.out.println("Nome do Produto:" + produtos.getString("nome"));
+
+			}
+		} catch (SQLException e) {
+			Logger.getLogger("Erro na conexão");
 		}
-		connection.close();
 	}
 }
