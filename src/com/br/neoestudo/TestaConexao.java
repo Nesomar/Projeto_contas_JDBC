d0624a66e554dcd545bd5df285dc6e96c78e0d26
Nesomar <nesomar.ramalho@outlook.com>
2019-07-21 19:13:11 -0300

    testando a conexão com o banco

--- src/com/br/neoestudo/TestaConexao.java
+++ src/com/br/neoestudo/TestaConexao.java
@@ -2,7 +2,9 @@ package com.br.neoestudo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaConexao {
 
@@ -10,6 +12,14 @@ public class TestaConexao {
 		
 		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/loja_virtual", "postgres", "1234");
 		System.out.println("Abrindo a conexão com o banco.");
+		
+		Statement statement = connection.createStatement();
+		ResultSet produtos = statement.executeQuery("SELECT * FROM Produto");
+		
+		while (produtos.next()) {
+			System.out.println("Nome do Produto:" + produtos.getString("nome"));
+			
+		}
 		connection.close();
 	}
 }
